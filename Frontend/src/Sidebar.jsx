2782f1061ebfce9a8c9a0743ea41bb518f845f6a
Vansh <sainivansh8116@gmail.com>
2025-08-27 23:35:29 +0530

    Ready to deployement

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -68,7 +68,7 @@ function Sidebar(){
             <section className="sidebar">
                 {/* new chat button */}
                 <button onClick={createNewChat}>
-                    <img src="src/assets/blacklogo.png" alt="logo" className="logo"/>
+                    <img src="public/assets/blacklogo.png" alt="logo" className="logo"/>
                     <span><i className="fa-solid fa-pen-to-square"></i></span>
                 </button>
 
